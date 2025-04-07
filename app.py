1dcdfa06aba897ce5f8e2c31843d628909338051
Alan Apanga <alan.nyange@yahoo.com>
2025-04-07 14:01:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -529,7 +529,6 @@ with col[1]:
             - :orange[**Negative Lead time**]: Implies that the item was shipped earlier than expected.
             - :orange[**Inbound Lead Time**]: Calculated using `createdDate` and `tranDate` from purchase orders data.
             - :orange[**Outbound On-Time Delivery Rate**]: Reflects alignment with planned schedules, not confirmed on-time deliveries, reason-missing `actual shipping dates`.
-.
             - :orange[**Filtering**]: Data is filtered for the **selected year**.
         ''')
 
