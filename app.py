201f92daf7c1c392b09860cf565ea79e7a73bda8
Alan Apanga <alan.nyange@yahoo.com>
2025-04-20 18:03:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,8 @@ Created on Mon Apr 14 22:43:53 2025
 """
 
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import altair as alt
-import plotly.express as px
 import glob
-import os
 import streamlit as st
 from datetime import timedelta
 
